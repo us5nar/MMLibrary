04df2ae1d8f093460bc5d40bd4156536b8062fa5
US5NAR <us5nar@gmail.com>
2016-08-20 01:08:49 +0300

    Added MediaFile.cs class

---
+++ MMLibrary/MediaFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMLibrary
+{
+    class MediaFile
+    {
+        public string FileName;
+        public int FileSize;
+        public string Title;
+        public string Singer;
+        public int Duration;
+    }
+}
